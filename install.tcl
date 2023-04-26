cdd9e808ec19ae656259a4b82bb19afa79d011c9
Alex Baker <48138840+ambaker1@users.noreply.github.com>
2023-04-26 12:26:55 -0500

    Renamed and updated installer

---
+++ install.tcl
@@ -0,0 +1,4 @@
+package require tin 0.4.1
+tin depend wob 0.1
+set dir [tin mkdir -force flytrap 0.1]
+file copy pkgIndex.tcl flytrap.tcl README.md LICENSE $dir
--- installer.tcl
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# the next line restarts using tclsh \
-exec tclsh "$0" ${1+"$@"}
-
-package require tin
-tin extract flytrap
