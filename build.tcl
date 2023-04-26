dec45a159fabf65f9f71f94d59b57768a95eb549
Alex Baker <48138840+ambaker1@users.noreply.github.com>
2023-04-26 12:27:20 -0500

    Create build.tcl
    Added build file which tests and installs flytrap

---
+++ build.tcl
@@ -0,0 +1,61 @@
+package require tin 0.4
+set config [dict create VERSION 0.1]
+tin bake src/flytrap.tin build/flytrap.tcl $config
+tin bake src/install.tin build/install.tcl $config
+tin bake src/pkgIndex.tin build/pkgIndex.tcl $config
+
+set dir build
+source build/pkgIndex.tcl
+package require flytrap
+namespace import flytrap::*
+
+# Assert value (throws error if not correct)
+flytrap -body {assert [expr {2 + 2}] == 4} 2 1
+
+# Ensure that the line numbers are correct
+assert [dict get [info frame 0] line] == 16
+
+proc foo {x} {
+    return [expr {1/$x}]
+}
+puts "Error should be on line 22"
+catch {flytrap -body {foo 0}}
+puts "Error should be on line 19"
+catch {flytrap -body {foo 0} 1}
+
+puts "Error should be on line 8 of error_example.tcl"
+catch {flytrap -body {
+    set a 5
+    source tests/error_example.tcl
+} 3}
+
+puts "Error should be on line 11 of error_example.tcl"
+catch {flytrap -file tests/error_example.tcl}
+puts "Error should be on line 4 of error_example.tcl"
+catch {flytrap -file tests/error_example.tcl 1}
+puts "Error should be on line 8 of error_example.tcl"
+catch {flytrap -file tests/error_example.tcl 2}
+puts "No error, but verbose"
+catch {flytrap -file tests/noerror_example.tcl 0 1}
+
+# Pause the script
+pause
+
+# Print variables
+set a 5
+set b 7
+set c(1) 5
+set c(2) 6
+pvar a b c
+
+# View all variables
+viewVars
+
+# Everything ok?
+puts "Update main files and install? (Y/N)"
+set result [gets stdin]
+if {$result eq "Y"} {
+    file copy -force {*}[glob -directory build *] [pwd]
+    tin bake doc/template/version.tin doc/template/version.tex $config
+    source install.tcl
+}
