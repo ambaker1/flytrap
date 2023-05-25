b4a9bbdcb5b65be376947355f115a1b6f53114f9
Alex Baker <48138840+ambaker1@users.noreply.github.com>
2023-05-25 17:49:27 -0500

    Update build.tcl
    All tests passed

--- build.tcl
+++ build.tcl
@@ -1,106 +1,149 @@
 package require tin 0.6
-set config [dict create VERSION 0.2]
+set config [dict create VERSION 0.1.3]
 tin bake src build $config
 
 set dir build
 source build/pkgIndex.tcl
-package require flytrap
-namespace import flytrap::*
+tin import flytrap
+set ::flytrap::DEBUG 1;# For testing
+set myLocation [file normalize [info script]]
 
-proc testPause {expected} {
-    set idleScript [list set expected $expected]
-    append idleScript {
-        puts ""
-        assert $::info eq $expected
-        set ::wob::userInput ""
-        set ::wob::userInputComplete 1
+# "pause" tests
+################################################################################
+assert [pause] eq "line 13 file \"$myLocation\""
+# Pause within proc
+proc foo {bar} {
+    global myLocation
+    puts $bar
+    assert [pause] eq "line 18 file \"$myLocation\""
+}
+foo foo
+
+# Pause within a nested proc with an eval
+proc foo {bar} {
+    global myLocation
+    bar $bar
+}
+proc bar {bar} {
+    puts $bar
+    uplevel 1 {
+        assert [pause] eq "line 30 file \"$myLocation\""
     }
-    after idle $idleScript
-    tailcall pause info
 }
+foo foo
 
-testPause "line 22 file \"[file normalize [info script]]\""
-
-# Assert value (throws error if not correct)
-flytrap -body {assert [expr {2 + 2}] == 4} 2 1
-
-# Ensure that the line numbers are correct
-assert [dict get [info frame 0] line] == 14
-
-proc foo {x} {
-    return [expr {1/$x}]
+# Pause within proc with multiple nested evals (with escaped newlines)
+proc foo {bar} {
+    puts $bar
+    uplevel 1 {
+        eval {
+            eval {
+                assert [\
+                        pause] eq "line 42 file \"$myLocation\""
+            }
+        }
+    }
 }
-puts "Error should be on line 20"
-catch {flytrap -body {foo 0} 0 0 options}
-assert [dict get $options
-puts "Error should be on line 17"
-catch {flytrap -body {foo 0} 1}
-
-puts "Error should be on line 8 of error_example.tcl"
-catch {flytrap -body {
-    set a 5
-    source tests/error_example.tcl
-} 3}
-
-puts "Error should be on line 11 of error_example.tcl"
-catch {flytrap -file tests/error_example.tcl}
-puts "Error should be on line 4 of error_example.tcl"
-catch {flytrap -file tests/error_example.tcl 1}
-puts "Error should be on line 8 of error_example.tcl"
-catch {flytrap -file tests/error_example.tcl 2}
-puts "No error, but verbose"
-catch {flytrap -file tests/noerror_example.tcl 0 1}
-
-# Pause the script
-pause
+foo foo
 
 # Pause within TclOO
-oo::class create example {
+# Note: constructor and destructor methods are an anomoly
+oo::class create example1 {
     constructor {args} {
-        pause
+        assert [pause] eq "line 2 method <constructor> class ::example1"
     }
-    method foo {} {
-        pause
+    method foo {bar} {
+        global myLocation
+        puts $bar
+        assert [pause] eq "line 58 file \"$::myLocation\""
     }
     destructor {
-        pause
+        assert [pause] eq "line 2 method <destructor> class ::example1"
     }
 }
-set x [example new]
-$x foo
+set x [example1 new]
+$x foo foo
 $x destroy
 
-# Pause within a procedure
+# Flytrap tests
+################################################################################
 
-# Pause within nested procedure with eval
+# Basic in-line error (and no error)
+assert [flytrap -body {assert [expr {2 + 2}] == 4} 2 1] eq ""
+assert [flytrap -body {
+    assert [expr {2 + 2}] == 3
+} 2 1] eq "line 74 file \"$myLocation\""
 
-proc hello {} {
-    set x 5
-    pause
-    return [hi]
+# Test error in nested procs (with eval)
+proc foo {x} {
+    bar $x
 }
-proc hi {} {
-    pause
+proc bar {x} {
+    expr {1/$x}
 }
+assert [flytrap -body {foo 0} 0 0] eq "line 84 file \"$myLocation\""
+assert [flytrap -body {foo 0} 1 0] eq "line 79 file \"$myLocation\""
+assert [flytrap -body {foo 0} 2 0] eq "line 82 file \"$myLocation\""
 
-hello
-hi
+# Flytrap files
+set error_example [file normalize tests/error_example.tcl]
+set noerror_example [file normalize tests/noerror_example.tcl]
+assert [flytrap -file $error_example] eq "line 11 file \"$error_example\""
+assert [flytrap -file $error_example 1] eq "line 4 file \"$error_example\""
+assert [flytrap -file $error_example 2] eq "line 7 file \"$error_example\""
+assert [flytrap -file $noerror_example 0 1] eq ""
 
+# Flytrap into methods.
+# Note: constructor and destructor methods are an anomoly
+oo::class create example2 {
+    constructor {args} {
+        expr 1/0
+    }
+}
+oo::class create example3 {
+    method foo {bar} {
+        expr 1/0
+    }
+    destructor {
+        expr 1/0
+    }
+}
+assert [flytrap -body {
+    set x [example2 new]
+} 1] eq "line 2 method <constructor> class ::example2"
+set x [example3 new]
+assert [flytrap -body {$x foo foo} 1] eq "line 105 file \"$::myLocation\""
+assert [flytrap -body {$x destroy} 1] eq "line 2 method <destructor> class ::example3"
+
+puts "TESTS PASSED."
+set ::flytrap::DEBUG 0
+puts "Flytrap with DEBUG off"
+catch {flytrap -body {expr 1/0}}
+assert $::flytrap::INFO eq "line 121 file \"$myLocation\""
+puts "Pause with DEBUG off"
+pause
 
 # Print variables
 set a 5
 set b 7
 set c(1) 5
 set c(2) 6
-pvar a b c
+set d(1) hello
+set d(2) world
+assert [::flytrap::PrintVars a b c d(1)] eq \
+{a = 5
+b = 7
+c(1) = 5
+c(2) = 6
+d(1) = hello}
+pvar a b c d(1); # for display
 
 # View all variables
 viewVars
 
 # Everything ok?
 puts "Update main files and install? (Y/N)"
-set result [gets stdin]
-if {$result eq "Y"} {
+if {[gets stdin] eq "Y"} {
     file copy -force {*}[glob -directory build *] [pwd]
     tin bake doc/template/version.tin doc/template/version.tex $config
     source install.tcl
